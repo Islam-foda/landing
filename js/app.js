567ec7cce21248e1822adddb8ce2de011323277f
Islam Fouda <83095505+Islam-foda@users.noreply.github.com>
2021-06-23 08:03:19 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,91 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const allSections = document.getElementsByTagName('section');
+const navMenu = document.getElementById('navbar__list')
+const frag = document.createDocumentFragment()
+
+
+
+
+/*
+ * Begin Main Functions
+ *
+ */
+//function to loop of All sections, and make likewise items at the navigation menu.
+function dynamicList() {
+    for (const sec of allSections) {
+        const secAttr = sec.getAttribute('data-nav')
+        const li = document.createElement('li')
+        li.setAttribute("class", "menu__link")
+        li.innerHTML = secAttr
+        frag.appendChild(li);
+        //console.log(secAttr)
+        li.addEventListener("click", function() {
+            sec.scrollIntoView({ behavior: "smooth" })
+        })
+    }
+    navMenu.appendChild(frag)
+}
+
+
+
+// build the nav
+dynamicList();
+
+// Add class 'active' to section when near top of viewport
+
+function setActiveClass() {
+    for (const sec of allSections) {
+        let goal = sec.getBoundingClientRect();
+
+        if (goal.top >= -250 && goal.top <= 500) {
+            sec.classList.add("your-active-class");
+            console.log("Section in the view")
+        } else { sec.classList.remove("your-active-class") };
+    }
+}
+window.addEventListener("scroll", function() {
+    setActiveClass();
+})
+
+
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
+const button = document.createElement('div')
+button.setAttribute('class', 'button');
+button.style.cssText("display: none; position: fixed; z-index: 10")
+document.body.appendChild(button);
